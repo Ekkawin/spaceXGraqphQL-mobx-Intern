c625690d2d3e1b99e32fdd62b7ac262e1aeab984
Aek <ekkawin.v@gmail.com>
2020-07-13 18:59:07 +0700

    done schema and endpoint server side

--- resolvers.js
+++ resolvers.js
@@ -3,29 +3,56 @@ const resolvers = {
   Query: {
     async launches(parent, args) {
       console.log(args.limit);
-      let res = await axios.get(
-        `https://api.spacexdata.com/v3/launches${
-          args.limit ? '?limit=' + args.limit : ''
-        }`
-      );
+      if (!args.limit) {
+        let res = await axios.get('https://api.spacexdata.com/v3/launches');
 
-      return res.data.map((e) => {
-        return {
-          flight_number: e.flight_number,
+        return res.data.map((e) => {
+          console.log(e.rocket.rocket_name);
+          return {
+            flight_number: e.flight_number,
 
-          mission_name: e.mission_name,
+            mission_name: e.mission_name,
 
-          launch_year: e.launch_year,
+            launch_year: e.launch_year,
 
-          launch_date_local: e.launch_date_local,
+            launch_date_local: e.launch_date_local,
 
-          launch_success: e.launch_success,
+            launch_success: e.launch_success,
 
-          //   rocket: {
+            rockets: {
+              rocket_id: e.rocket.rocket_id,
+              rocket_name: e.rocket.rocket_name,
+              rocket_type: e.rocket.rocket_type,
+            },
+          };
+        });
+      } else {
+        console.log(args.limit, 'hihihihihihihihi');
+        let res = await axios.get(
+          `https://api.spacexdata.com/v3/launches?limit=${args.limit}`
+        );
+        console.log('hiiii');
+        return res.data.map((e) => {
+          console.log(e.rocket.rocket_name);
+          return {
+            flight_number: e.flight_number,
 
-          //   },
-        };
-      });
+            mission_name: e.mission_name,
+
+            launch_year: e.launch_year,
+
+            launch_date_local: e.launch_date_local,
+
+            launch_success: e.launch_success,
+
+            rockets: {
+              rocket_id: e.rocket.rocket_id,
+              rocket_name: e.rocket.rocket_name,
+              rocket_type: e.rocket.rocket_type,
+            },
+          };
+        });
+      }
     },
     async launch(parent, args) {
       console.log('args', args.flight_number);
